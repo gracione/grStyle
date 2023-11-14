4d3c3ae5c4335aa7ebd23f576f8f71acca1ceb4d
gracione <gracione2017@gmail.com>
2023-11-14 06:03:27 -0300

    consultar

--- cliente/src/services/queries.ts
+++ cliente/src/services/queries.ts
@@ -10,6 +10,11 @@ export const consultarDados = async (parametros: string): Promise<any> => {
   }
 };
 
+export const consultar = async (parametros: string): Promise<any> => {
+  const resposta = await api.get(parametros);
+  return resposta.data;
+}
+
 export const inserirDados = async (dados: any): Promise<any> => {
   try {
     const resposta = await api.post('/inserir', dados);
